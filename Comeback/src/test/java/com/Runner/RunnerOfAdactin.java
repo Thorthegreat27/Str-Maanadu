f35cc3dc1c1e538735127eb2feb686f80b080c14
Thorthegreat27 <annamalaikumar270595@gmail.com>
2023-01-12 07:54:17 +0530

    initial commit

---
+++ Comeback/src/test/java/com/Runner/RunnerOfAdactin.java
@@ -0,0 +1,15 @@
+package com.Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features="C:\\Users\\ANNAMALAI\\eclipse-workspace\\2022-Dec\\Comeback\\src\\test\\resources\\com\\Feature\\",glue="com.Stepdefinition")
+
+
+public class RunnerOfAdactin {
+
+}
